1361e228e96c3e13db321757b7a9d7a03972bcce
zhushimao123 <152359342622@163.com>
2019-05-17 06:31:08 +0000

    购物车

--- app/Http/Controllers/passport/PassportController.php
+++ app/Http/Controllers/passport/PassportController.php
@@ -116,8 +116,9 @@ class PassportController extends Controller
         }else{
             //做修改
             //根据商品id 用户id
-            $info = DB::table('shop_cart')->where(['goods_id'=> $json_arr['goods_id'],'user_id'=> $json_arr['user_id']])->get()->toArray();
-            $buy_number = $info[0]['buy_number'];
+            $info = DB::table('shop_cart')->where(['goods_id'=> $json_arr['goods_id'],'user_id'=> $json_arr['user_id']])->first();
+            var_dump($info);die;
+            $buy_number = $info->buy_number;
             var_dump($buy_number);die;
             $res = DB::table('shop_cart')->where(['goods_id'=> $json_arr['goods_id'],'user_id'=> $json_arr['user_id']])->update(['buy_number'=>$buy_number + $json_arr['buy_number']]);
             if($res){
